89414b511f0eeeef3e9b38a06ec096c625e6235a
M-Rime <queromais.uba@gmail.com>
2023-08-07 14:04:19 -0300

    .

--- _1006/Media_2.java
+++ _1006/Media_2.java
@@ -28,15 +28,15 @@ public class Media_2 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            double a = sc.nextDouble() * 2;
+            double b = sc.nextDouble() * 3;
+            double c = sc.nextDouble() * 5;
+            double media = (a + b + c) / 10;
+
+            System.out.printf("MEDIA = %.1f%n", media);
+        }
 
         /* Notas */
-
-        double a = sc.nextDouble() * 2;
-        double b = sc.nextDouble() * 3;
-        double c = sc.nextDouble() * 5;
-        double media = (a + b + c) / 10;
-
-        System.out.printf("MEDIA = %.1f%n", media);
     }
 }
