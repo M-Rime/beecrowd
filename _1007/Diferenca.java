72d9969fd98a40e3150d0be4b6f937c09c7947c7
M-Rime <queromais.uba@gmail.com>
2023-08-08 08:03:38 -0300

    Accepted

--- _1007/Diferenca.java
+++ _1007/Diferenca.java
@@ -1,7 +1,8 @@
 /* 
  * 
- *  Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
-
+ *  Leia quatro valores inteiros A, B, C e D.
+ *  A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+ *
  * Entrada
  * O arquivo de entrada contém 4 valores inteiros.
  * 
@@ -21,11 +22,22 @@
  */
 package _1007;
 
+import java.util.Scanner;
+
 public class Diferenca {
 
     public static void main(String[] args){
         
-        /* ... */
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        int dif = ( (a * b) - ( c * d ) );
+
+        System.out.println("DIFERENCA = " + dif );
     }
-    
+
 }
