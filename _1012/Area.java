08ef69bd67968a716c17b4fe54fa2ed575d49d23
M-Rime <queromais.uba@gmail.com>
2023-08-09 13:28:11 -0300

    Accepted

--- _1012/Area.java
+++ _1012/Area.java
@@ -1,10 +1,19 @@
 /* 
  * Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
- * a) a área do triângulo retângulo que tem A por base e C por altura.
- * b) a área do círculo de raio C. (pi = 3.14159)
- * c) a área do trapézio que tem A e B por bases e C por altura.
- * d) a área do quadrado que tem lado B.
+ * 
+ * [OK] a) a área do triângulo retângulo que tem A por base e C por altura. -> [base * altura / 2 ].
+ * 
+ * [OK] b) a área do círculo de raio C. (pi = 3.14159) - > [ pir * raio^2 ].
+ * 
+ * [OK] c) a área do trapézio que tem A e B por bases e C por altura. -> ( ( Base maior + Base Meor ) * Altura ) / 2 
+ *                                                                    Para calcular a área de um trapézio basta somar a base maior e base menor,
+ *                                                                    multiplicar pela altura e depois, basta dividir o resultado por dois.
+ * 
+ * d) a área do quadrado que tem lado B. -> A área do quadrado, um caso particular de quadrilátero, pode ser calculada elevando seu lado ao quadrado.
+ *                                          A área do quadrado é igual à medida do seu lado ao quadrado.
+ * 
  * e) a área do retângulo que tem lados A e B.
+ * 
  * Entrada
  * O arquivo de entrada contém três valores com um dígito após o ponto decimal.
  * 
@@ -22,6 +31,31 @@
 
 package _1012;
 
+import java.util.Scanner;
+
 public class Area {
+
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
+            double c = sc.nextDouble();
+            double pi = 3.14159;
+
+            double triangulo = (a * c) / 2;
+            double circulo = pi * Math.pow(c, 2);
+            double trapezio = ((a + b) * c) / 2;
+            double quadrado = Math.pow(b, 2);
+            double retangulo = a * b;
+
+            System.out.printf("TRIANGULO: %.3f%n", triangulo);
+            System.out.printf("CIRCULO: %.3f%n", circulo);
+            System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+            System.out.printf("QUADRADO: %.3f%n", quadrado);
+            System.out.printf("RETANGULO: %.3f%n", retangulo);
+        }
+
+    }
 
 }
