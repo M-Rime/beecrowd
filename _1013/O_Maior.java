e93483ec0f63de8a9559906c04043cbf0cb3c00e
M-Rime <queromais.uba@gmail.com>
2023-08-09 16:14:03 -0300

    Accepted

--- _1013/O_Maior.java
+++ _1013/O_Maior.java
@@ -25,6 +25,33 @@
 
 package _1013;
 
+import java.util.Scanner;
+
 public class O_Maior {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int maior;
+
+        if (a > b & a > c) {
+
+            maior = a;
+
+        } else if (b > c) {
+
+            maior = b;
+
+        } else {
+
+            maior = c;
+        }
+
+        System.out.println(maior + " eh o maior");
+    }
 
 }
