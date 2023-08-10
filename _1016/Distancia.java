0ae330c7a09a4ea7683043719a00df0f889bf8a8
M-Rime <queromais.uba@gmail.com>
2023-08-10 16:27:49 -0300

    Accepted

--- _1016/Distancia.java
+++ _1016/Distancia.java
@@ -1,6 +1,9 @@
 /* 
+ * Dois carros (X e Y) partem em uma mesma direção.
  * 
- * Dois carros (X e Y) partem em uma mesma direção. O carro X sai com velocidade constante de 60 Km/h e o carro Y sai com velocidade constante de 90 Km/h.
+ * O carro X sai com velocidade constante de 60 Km/h e o 
+ *   carro Y sai com velocidade constante de 90 Km/h.
+ * 
  * Em uma hora (60 minutos) o carro Y consegue se distanciar 30 quilômetros do carro X, ou seja, consegue se afastar um quilômetro a cada 2 minutos.
  * 
  * Leia a distância (em Km) e calcule quanto tempo leva (em minutos) para o carro Y tomar essa distância do outro carro.
@@ -20,6 +23,23 @@
 
 package _1016;
 
+import java.util.Scanner;
+
 public class Distancia {
-    
+
+    public static void main(String[] args) {
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            int km = sc.nextInt();
+
+            double km_Minuto = 0.5;
+
+            double tempo = (km / km_Minuto);
+
+            System.out.printf("%.0f minutos%n",tempo);
+
+        }
+
+    }
 }
