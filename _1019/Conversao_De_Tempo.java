db12fe45bbd2c7d3adc396ede27ec38497def2c2
M-Rime <queromais.uba@gmail.com>
2023-08-14 16:59:52 -0300

    Pedente Termino

--- _1019/Conversao_De_Tempo.java
+++ _1019/Conversao_De_Tempo.java
@@ -6,16 +6,45 @@
  * Saída
  * Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
  * 
- * Exemplo de Entrada	Exemplo de Saída
- * 556
- * 
- * 0:9:16
+ * Exemplo de Entrada |	Exemplo de Saída
+ *               556  | 0:9:16
  * 
 */
 
-
 package _1019;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Conversao_De_Tempo {
-    
+    public static void main(String[] args) {
+
+        DecimalFormat df = new DecimalFormat("#,###.000");
+
+        try (Scanner sc = new Scanner(System.in)) {
+
+            double entrada = sc.nextInt();
+            double horas = 0;
+            double minutos = 0;
+            double segundos = 0;
+
+            double temp = 0;
+
+            horas = entrada / 3600;
+            
+            
+
+            minutos = entrada / 60;
+            segundos = (minutos / 60);
+
+            System.out.printf("%f%n", horas);
+            System.out.printf("%f%n", minutos);
+            System.out.printf("%fn",segundos);
+
+            System.out.println("Temp - " + temp);
+
+        }
+
+    }
+
 }
