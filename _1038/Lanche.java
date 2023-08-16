ff1077361e1650c6ef3771b00cef93fd2a1cadbe
M-Rime <queromais.uba@gmail.com>
2023-08-16 13:53:41 -0300

    Exercico Adicionado

---
+++ _1038/Lanche.java
@@ -0,0 +1,34 @@
+/* 
+ *                                                                             Lanche
+ * 
+ *      Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
+ * 
+ *      __________________________________________
+ *     | CODIGO  |  ESPECIFICAÇÃO   |  PREÇO      |
+ *     |------------------------------------------|
+ *     |     1   | CACHORRO QUENTE  | R$ 4.00     |
+ *     |     2   | X-SALADA         | R$ 4.50     |
+ *     |     3   | X-BACON          | R$ 5.00     |
+ *     |     4   | TORRADA SIMPLES  | R$ 2.00     |
+ *     |     5   | REFRIGERANTE     | R$ 1.50     |
+ *     --------------------------------------------    
+ * 
+ *   Entrada
+ *  O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
+ * 
+ *   Saída
+ *  O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
+ * 
+ * 
+ * Exemplo de Entrada | Exemplo de Entrada | 
+ *             3 2    | Total: R$ 10.00
+ * 
+*/
+
+
+
+package _1038;
+
+public class Lanche {
+    
+}
---
+++ _1041/Coordenadas_De_Um_Ponto.java
@@ -0,0 +1,37 @@
+/* 
+ *                                                              Coordenadas de um Ponto
+ * 
+ *    Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano.
+ *        A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+ *                                                                     
+ *                                                                     | y         
+ *                                                                     |
+ *                                                                Q2   |   Q1
+ *                                                                     |        x                                           
+ *                                                           ----------|----------
+ *                                                                     |                                                   
+ *                                                                Q3   |   Q4
+ *                                                                     |
+ *                                                                     |
+ *
+ *
+ *    Se o ponto estiver na origem, escreva a mensagem “Origem”.
+ *      Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
+ *
+ *  Entrada
+ *      A entrada contem as coordenadas de um ponto.
+ *
+ *  Saída
+ *      A saída deve apresentar o quadrante em que o ponto se encontra.
+ *
+ *    Exemplo de Entrada  | Exemplo de Saída
+ *        4.5 -2.2        |       Q4
+ *
+ */
+
+
+package _1041;
+
+public class Coordenadas_De_Um_Ponto {
+    
+}
