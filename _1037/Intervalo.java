f1fd604787ff7635973bf150b27493e6c7b105b5
M-Rime <queromais.uba@gmail.com>
2023-08-25 09:22:26 -0300

    Accepted

--- _1037/Intervalo.java
+++ _1037/Intervalo.java
@@ -1,5 +1,7 @@
 /* 
- *  Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+ *  Você deve fazer um programa que leia um valor qualquer
+ *  e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. 
+ *  Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
  *   
  *   O símbolo ( representa "maior que". Por exemplo:
  *   [0,25]  indica valores entre 0 e 25.0000, inclusive eles.
@@ -22,10 +24,33 @@
  * 
 */
 
-
-
 package _1037;
 
+import java.util.Scanner;
+
 public class Intervalo {
-    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double valor_Q = sc.nextDouble();
+
+        if (valor_Q >= 0 && valor_Q <= 25) {
+            System.out.println("Intervalo [0,25]");
+
+        } else if (valor_Q > 25 && valor_Q <= 50) {
+            System.out.println("Intervalo (25,50]");
+
+        } else if (valor_Q > 50 && valor_Q <= 75) {
+            System.out.println("Intervalo (50,70]");
+
+        } else if (valor_Q > 75 && valor_Q <= 100) {
+            System.out.println("Intervalo (75,100]");
+
+        } else {
+            System.out.println("Fora de intervalo");
+        }
+
+    }
+
 }
