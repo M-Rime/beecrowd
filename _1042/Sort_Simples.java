cf795af670004ce3b8c8ee36cea2641ae1b35501
M-Rime <queromais.uba@gmail.com>
2023-08-30 11:24:36 -0300

    Accepted

--- _1042/Sort_Simples.java
+++ _1042/Sort_Simples.java
@@ -25,9 +25,43 @@
  * 
  */
 
-
 package _1042;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Sort_Simples {
-    
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            ArrayList<Integer> lista = new ArrayList<>();
+            ArrayList<Integer> lista2 = new ArrayList<>();
+
+            int i = 0;
+
+            while (i != 3) {
+
+                lista.add(sc.nextInt());
+                lista2.add(lista.get(i));
+                i++;
+            }
+
+            lista.sort(null);
+
+            for (Integer x : lista) {
+
+                System.out.println(x);
+            }
+
+            System.out.println("");
+
+            for (Integer x : lista2) {
+
+                System.out.println(x);
+            }
+
+        }
+    }
 }
+
+// for (int y = lista.size() - 1; y >= 0; y--) {
+// system.out.println(lista.get(y)); }
